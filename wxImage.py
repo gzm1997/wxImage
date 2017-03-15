c9e880c0b4c8be56d31dc86e597b0773a12fe7aa
一条咸鱼的自我修养 <1617899539@qq.com>
2017-03-15 00:49:23 -0500

    Add files via upload

---
+++ wxImage.py
@@ -0,0 +1,75 @@
+from numpy import *
+import itchat
+import urllib
+import requests
+import os
+
+import PIL.Image as Image
+from os import listdir
+import math
+
+
+myheaders = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+
+myRequests = requests.Session()
+myRequests.headers.update(myheaders)
+
+itchat.auto_login(enableCmdQR=True)
+
+friends = itchat.get_friends(update=True)[0:]
+
+user = friends[0]["UserName"]
+
+print(user)
+
+os.mkdir(user)
+
+num = 0
+
+for i in friends:
+	img = itchat.get_head_img(userName=i["UserName"])
+	fileImage = open(user + "/" + str(num) + ".jpg",'wb')
+	fileImage.write(img)
+	fileImage.close()
+	num += 1
+
+pics = listdir(user)
+
+numPic = len(pics)
+
+print(numPic)
+
+eachsize = int(math.sqrt(float(640 * 640) / numPic))
+
+print(eachsize)
+
+numline = int(640 / eachsize)
+
+toImage = Image.new('RGBA', (640, 640))
+
+
+print(numline)
+
+x = 0
+y = 0
+
+for i in pics:
+	try:
+		img = Image.open(user + "/" + i)
+	except IOError:
+		print("Error: 没有找到文件或读取文件失败")
+	else:
+		img = img.resize((eachsize, eachsize), Image.ANTIALIAS)
+		toImage.paste(img, (x * eachsize, y * eachsize))
+		x += 1
+		if x == numline:
+			x = 0
+			y += 1
+
+
+toImage.save(user + ".jpg")
+
+
+itchat.send_image(user + ".jpg", 'filehelper')
+
+
